6b22163ce1d2a273c918dae838ae47246fc03583
Toan Le <toanle@Toans-Mac-mini.local>
2026-07-22 18:17:57 +0700

    fix: add CSP for CKEditor to fix infinite loading spinner

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -9,9 +9,11 @@ const config: Core.Config.Middlewares = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'connect-src': ["'self'", 'https:', 'https://proxy-event.ckeditor.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'https://cdn.ckeditor.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://cdn.ckeditor.com'],
+          'frame-src': ["'self'", 'https://cdn.ckeditor.com'],
           upgradeInsecureRequests: null,
         },
       },
