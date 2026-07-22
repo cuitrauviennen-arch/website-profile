718e45b3d59fdc2132b9c18933580ccfc990f46e
Toan Le <toanle@Toans-Mac-mini.local>
2026-07-22 17:55:44 +0700

    fix: correct CKEditor plugin name registration

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -16,6 +16,10 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
       },
     },
   },
+  ckeditor5: {
+    enabled: true,
+    resolve: './node_modules/@_sh/strapi-plugin-ckeditor'
+  },
 });
 
 export default config;
